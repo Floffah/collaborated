0fe3732800858301895bd227258e29a59623bec5
Floffah <therealfloffah@gmail.com>
2021-03-30 16:14:23 +0100

    dont use webpack 5 until out of beta

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,15 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const i18n = require("./next-i18next.config");
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-//const tm = require("next-transpile-modules")(["@collaborated/interact"]);
+const tm = require("next-transpile-modules")(["@collaborated/interact"]);
 
-module.exports = {
-    //tm({
+module.exports = tm({
     reactStrictMode: true,
     trailingSlash: true,
     productionBrowserSourceMaps: true,
     ...i18n,
-    future: {
-        webpack5: true,
-    },
-}; //);
+    // future: {
+    //     webpack5: true, - reenable if this doesnt fix next-transpile-modules
+    // },
+});
