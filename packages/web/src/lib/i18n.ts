dcb0f719c8a095a5a3b1c64e2ff38d51e8c45fdb
Floffah <therealfloffah@gmail.com>
2021-03-30 17:01:57 +0100

    will netlify just deploy pls

--- packages/web/src/lib/i18n.ts
+++ packages/web/src/lib/i18n.ts
@@ -1,9 +1,7 @@
 import { State } from "./store";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { GetServerSidePropsContext, GetStaticPropsContext } from "next";
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const i18nconfig = require("@collaborated/web/next-i18next.config.js");
+import { resolve } from "path";
 
 export interface Props {
     initialState: State;
@@ -15,8 +13,11 @@ export const SST = (
     p: GetServerSidePropsContext | GetStaticPropsContext,
     ns?: string[],
 ) =>
-    serverSideTranslations(
-        p.locale as string,
-        ns ?? localeNamespaces,
-        i18nconfig,
-    );
+    serverSideTranslations(p.locale as string, ns ?? localeNamespaces, {
+        localePath: resolve("./public/locale"),
+        defaultNS: "common",
+        i18n: {
+            locales: ["en"],
+            defaultLocale: "en",
+        },
+    });
