5f3650da9439f6a8ee2e7de7ab58c60fd72fd3b0
Floffah <therealfloffah@gmail.com>
2021-03-31 17:06:58 +0100

    dependency bump, textinput sizing fix, button disabled darken

--- server/lib/capp/router.ex
+++ server/lib/capp/router.ex
@@ -1,13 +1,12 @@
 defmodule CappBackend.Router do
   use Plug.Router
+  import Plug.Conn
 
   alias CappBackend.StaticPlug
 
   plug Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json, Absinthe.Plug.Parser],
     json_decoder: Jason
-
-  plug StaticPlug
 
   plug Absinthe.Plug,
        schema: CappBackend.APISchema,
@@ -17,6 +16,7 @@ defmodule CappBackend.Router do
   plug :dispatch
 
   match _ do
+    IO.puts(conn.request_path)
     conn
     |> put_resp_content_type("application/json")
     |> send_resp(404, "{\"error\": \"Not found\"}")
