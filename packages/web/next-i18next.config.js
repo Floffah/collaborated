04647cba974fbd3fcf33121204d74383ac997c28
Floffah <therealfloffah@gmail.com>
2021-04-05 10:31:49 +0100

    reenable suspense

--- packages/web/next-i18next.config.js
+++ packages/web/next-i18next.config.js
@@ -4,18 +4,18 @@ const path = require("path");
 const locales = ["en"];
 
 module.exports = {
-    ns: ["common", "errors", "login", "seo"],
+    // ns: ["common", "errors", "login", "seo"],
     i18n: {
         defaultLocale: "en",
         locales,
     },
-    defaultNS: "common",
-    react: {
-        useSuspense: false,
-    },
-    preload: locales,
+    // defaultNS: "common",
+    // react: {
+    //     useSuspense: false,
+    // },
+    // preload: locales,
     localePath: path.resolve("./public/locales"),
-    cleanCode: true,
-    fallbackNS: ["common", "errors", "seo"],
-    fallbackLng: "en",
+    // cleanCode: true,
+    // fallbackNS: ["common", "errors", "seo"],
+    // fallbackLng: "en",
 };
